da2106e078cc5fc003554aea5f7563f3a6a7cda5
Nick <ghost2394228@gmail.com>
2022-12-09 14:35:25 +0300

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
-const routes = require("./routes/routes");
+const routes = require("./routes/loginRoutes");
 
 const { PORT = 3000 } = process.env;
 
